c375fb5fb23e5afd8b3123b5bb998949ee5a3f30
jairo <azulmarino-jairo22@outlook.com>
2018-08-04 23:44:58 -0500

    proyecto

---
+++ android_yolo_master/app/src/main/java/org/tensorflow/demo/Clases/VariablesYDatos.java
@@ -0,0 +1,85 @@
+package org.tensorflow.demo.Clases;
+
+
+import org.tensorflow.demo.R;
+
+/**
+ * Created by azulm on 17/07/2018.
+ */
+
+public class VariablesYDatos {
+
+
+   public static  String cifras []={"En 2010, el número estimado de personas con discapacidad visual en todo el mundo ascendía a 285\n millones, de las cuales 39 millones eran ciegas. Esto supone una disminución respecto del número de personas con discapacidad\n visual estimado en 2004. Ello puede deberse a datos de mejor calidad, pero también a las intervenciones que\n han permitido reducir el número de personas con discapacidad visual evitable.",
+            "El 90% de las personas con discapacidad visual vive en países de ingresos bajos\n" +
+                    "Para esas personas, el acceso a servicios de prevención, educación y tratamiento, así como de rehabilitación,\n aún no es universal.",
+            "El 28% de las personas que padecen discapacidad visual moderada y grave están edad de trabajar\n" +
+                    "Las limitaciones visuales afectan la capacidad de los trabajadores para llevar una vida productiva.\n Esto menoscaba sus posibilidades de encontrar empleo y mantenerse a sí mismos y a sus familias.",
+            "Reducir las discapacidades visuales en un 25% para 2019\n" +
+                    "La 66.a Asamblea Mundial de la Salud aprobó un plan de acción destinado a reducir las discapacidades visuales en un\n 25% para 2019. Este es un objetivo alcanzable que requiere la colaboración de gobiernos, organismos de desarrollo, sector\n privado, ONG y por su puesto, nosotros ayudados a mejorar la calidad de vida.",
+            "Unos 1,4 millones de niños son ciegos\n" +
+                    "Las principales causas de ceguera entre los niños son las cataratas, la retinopatía de la prematuridad\n y la carencia de vitamina A. Aproximadamente la mitad de todas las cegueras infantiles se pueden evitar o tratar.",
+            "Se estima que 120 millones de personas padecen discapacidad visual por errores de refracción no corregidos\n" +
+                    "Los errores de refracción (hipermetropía y miopía) se encuentran entre los casos de discapacidad visual más fácilmente\n corregibles."
+    };
+  public static  Object etiquetas[][]={
+            {"como", R.drawable.como},
+            {"cual",R.drawable.cual},
+            {"cuando",R.drawable.cuando},
+            {"cuanto",R.drawable.cuanto},
+            {"donde",R.drawable.donde},
+            {"para que",R.drawable.paraque},
+            {"por que",R.drawable.porque},
+            {"que",R.drawable.que},
+            {"quien",R.drawable.quen},
+            {"bien",R.drawable.bien},
+            {"mal",R.drawable.mal},
+            {"buenasnoches",R.drawable.buenasnoches},
+            {"buenastardes",R.drawable.buenastardes},
+            {"buenosdias",R.drawable.buenosdias},
+            {"comoestas",R.drawable.comoestas},
+            {"denada",R.drawable.denada},
+            {"gracias",R.drawable.gracias},
+            {"gustoenconocerte",R.drawable.gustoenconocerte},
+            {"hola",R.drawable.hola},
+            {"mal",R.drawable.mal},
+            {"masomenos",R.drawable.masomenos},
+            {"nosvemos",R.drawable.nosvemos},
+            {"tunombre",R.drawable.tunombre},
+
+          {"aveces",R.drawable.aveces},
+          {"ahorita",R.drawable.ahorita},
+          {"antes",R.drawable.antes},
+          {"año",R.drawable.ano},
+          {"aun",R.drawable.aun},
+          {"ayer",R.drawable.ayer},
+          {"constante",R.drawable.constante},
+          {"dia",R.drawable.dia},
+          {"despues",R.drawable.despues},
+          {"enadelante",R.drawable.enadelante},
+          {"futuro",R.drawable.futuro},
+          {"hora",R.drawable.hora},
+          {"hoy",R.drawable.hoy},
+          {"jamas",R.drawable.jamas},
+          {"mañana",R.drawable.manana},
+          {"mediodia",R.drawable.mediodia},
+          {"mes",R.drawable.mes},
+          {"mientras",R.drawable.mientras},
+          {"minuto",R.drawable.minuto},
+          {"noche",R.drawable.noche},
+          {"nunca",R.drawable.nunca},
+          {"otravez",R.drawable.otravez},
+          {"pasado",R.drawable.pasado},
+          {"proximo",R.drawable.proximo},
+          {"repetir",R.drawable.repetir},
+          {"seguir",R.drawable.seguir},
+          {"semana",R.drawable.semana},
+          {"siempre",R.drawable.siempre},
+          {"tarde",R.drawable.tarde},
+          {"tiempo",R.drawable.tiempo},
+          {"todaviano",R.drawable.todaviano},
+          {"unavez",R.drawable.unavez},
+          {"primeravez",R.drawable.primeravez}
+    };
+
+}
---
+++ android_yolo_master/app/src/main/java/org/tensorflow/demo/Fragment_Comunicativo2.java
@@ -0,0 +1,29 @@
+package org.tensorflow.demo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_Comunicativo2 extends Fragment {
+
+
+    public Fragment_Comunicativo2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment__comunicativo2, container, false);
+    }
+
+}
